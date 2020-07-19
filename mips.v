acdba9e6c9d55728ca9aff6aece9fb8e2b650863
vostolaza <victor.ostolaza@utec.edu.pe>
2020-07-19 17:11:24 -0500

    Single Cycle

---
+++ mips.v
@@ -0,0 +1,143 @@
+`include "alu.v"
+`include "mipsparts.v"
+
+module mips(input         clk, reset,
+            output [31:0] pc,
+            input  [31:0] instr,
+            output        memwrite,
+            output [31:0] aluout, writedata,
+            input  [31:0] readdata);
+
+  wire        memtoreg, branch,
+              pcsrc, zero,
+              alusrc, regdst, regwrite, jump;
+  wire [2:0]  alucontrol;
+
+  controller c(instr[31:26], instr[5:0], zero,
+               memtoreg, memwrite, pcsrc,
+               alusrc, regdst, regwrite, jump,
+               alucontrol);
+  datapath dp(clk, reset, memtoreg, pcsrc,
+              alusrc, regdst, regwrite, jump,
+              alucontrol,
+              zero, pc, instr,
+              aluout, writedata, readdata);
+endmodule
+
+module controller(input  [5:0] op, funct,
+                  input        zero,
+                  output       memtoreg, memwrite,
+                  output       pcsrc, alusrc,
+                  output       regdst, regwrite,
+                  output       jump,
+                  output [2:0] alucontrol);
+
+  wire [1:0] aluop;
+  wire       branch;
+  wire       beq;
+
+  maindec md(op, memtoreg, memwrite, branch,
+             alusrc, regdst, regwrite, jump,
+             aluop);
+  assign beq = (op == 6'b000100) ? 1 : 0;
+  aludec  ad(funct, aluop, beq, alucontrol);
+
+  assign pcsrc = branch & zero;
+endmodule
+
+module maindec(input  [5:0] op,
+               output       memtoreg, memwrite,
+               output       branch, alusrc,
+               output       regdst, regwrite,
+               output       jump,
+               output [1:0] aluop);
+
+  reg [8:0] controls;
+
+  assign {regwrite, regdst, alusrc,
+          branch, memwrite,
+          memtoreg, jump, aluop} = controls;
+
+  always@(*)
+    case(op)
+      6'b000000: controls <= 9'b110000010; //Rtype
+      6'b100011: controls <= 9'b101001000; //LW
+      6'b101011: controls <= 9'b001010000; //SW
+      6'b000100: controls <= 9'b000100001; //BEQ
+      6'b001000: controls <= 9'b101000000; //ADDI
+      6'b000010: controls <= 9'b000000100; //J
+      6'b000101: controls <= 9'b000100001; //BNE
+      6'b001101: controls <= 9'b101000011; //ORI
+      default:   controls <= 9'bxxxxxxxxx; //???
+    endcase
+endmodule
+
+module aludec(input  [5:0] funct,
+              input  [1:0] aluop,
+              input beq,
+              output reg [2:0] alucontrol);
+
+  always@(*)
+    case(aluop)
+      2'b00: alucontrol <= 3'b010;  // add
+      2'b01: alucontrol <= beq ? 3'b011 :  3'b110;  // sub, bne
+      2'b11: alucontrol <= 3'b001;  // or
+      default: case(funct)          // 10 RTYPE
+          6'b100000: alucontrol <= 3'b010; // ADD
+          6'b100010: alucontrol <= 3'b110; // SUB
+          6'b100100: alucontrol <= 3'b000; // AND
+          6'b100101: alucontrol <= 3'b001; // OR
+          6'b101010: alucontrol <= 3'b111; // SLT
+          default:   alucontrol <= 3'bxxx; // ???
+        endcase
+    endcase
+endmodule
+
+module datapath(input         clk, reset,
+                input         memtoreg, pcsrc,
+                input         alusrc, regdst,
+                input         regwrite, jump,
+                input  [2:0]  alucontrol,
+                output        zero,
+                output [31:0] pc,
+                input  [31:0] instr,
+                output [31:0] aluout, writedata,
+                input  [31:0] readdata);
+
+  wire [4:0]  writereg;
+  wire [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
+  wire [31:0] signimm, signimmsh;
+  wire [31:0] srca, srcb;
+  wire [31:0] result;
+
+  // next PC logic
+  flopr #(32) pcreg(clk, reset, pcnext, pc);
+  adder       pcadd1(pc, 32'b100, pcplus4);
+  sl2         immsh(signimm, signimmsh);
+  adder       pcadd2(pcplus4, signimmsh, pcbranch);
+  mux2 #(32)  pcbrmux(pcplus4, pcbranch, pcsrc,
+                      pcnextbr);
+  mux2 #(32)  pcmux(pcnextbr, {pcplus4[31:28],
+                    instr[25:0], 2'b00},
+                    jump, pcnext);
+
+  // register file logic
+  regfile     rf(clk, regwrite, instr[25:21],
+                 instr[20:16], writereg,
+                 result, srca, writedata);
+  mux2 #(5)   wrmux(instr[20:16], instr[15:11],
+                    regdst, writereg);
+  mux2 #(32)  resmux(aluout, readdata,
+                     memtoreg, result);
+
+  wire sel;
+  assign sel = instr[31:26] == 6'b001101 ? 1 : 0;
+
+  signext     se(instr[15:0], sel, signimm);
+
+  // ALU logic
+  mux2 #(32)  srcbmux(writedata, signimm, alusrc,
+                      srcb);
+  alu         alu(.a(srca), .b(srcb), .f(alucontrol),
+                  .y(aluout), .zero(zero));
+endmodule
---
+++ mips_tb2.v
@@ -0,0 +1,42 @@
+module testbench();
+
+  reg        clk;
+  reg        reset;
+
+  wire [31:0] writedata, dataadr;
+  wire        memwrite;
+
+  // instantiate device to be tested
+  top dut(clk, reset, writedata, dataadr, memwrite);
+
+  // initialize test
+  initial
+    begin
+      reset <= 1; # 10; reset <= 0;
+    end
+
+  // generate clock to sequence tests
+  always
+    begin
+      clk <= 1; # 5; clk <= 0; # 5;
+    end
+
+  // check that 7 gets written to address 84
+  always@(negedge clk)
+    begin
+      if(memwrite) begin
+        if(writedata == 32'hFFFF7F02 && dataadr == 83) begin
+          $display("Simulation succeeded");
+          $stop;
+        end else if (dataadr != 80) begin
+          $display("Simulation failed");
+          $stop;
+        end
+      end
+    end
+initial begin
+        $dumpfile("mips2.vcd");
+        $dumpvars;
+end
+
+endmodule
